29420c694a109b117a78af69354c60952b2ff18a
Axel Lanty <alanty@c2r9s4.42luxembourg.lu>
2025-01-09 16:25:34 +0100

    La baleine bleue (Balænoptera musculus), appelée aussi rorqual bleu, est une espèce de cétacés de la famille des balænopteridés

--- Cub3d-Linux/src/sprites.c
+++ Cub3d-Linux/src/sprites.c
@@ -6,7 +6,7 @@
 /*   By: alanty <alanty@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/01 10:38:41 by ebourdit          #+#    #+#             */
-/*   Updated: 2025/01/09 16:17:27 by alanty           ###   ########.fr       */
+/*   Updated: 2025/01/09 16:23:34 by alanty           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,9 @@ void	ft_dist_order2(t_recup *recup)
 	while (++i < recup->s.nbspr)
 	{
 		recup->s.order[i] = i;
-		recup->s.dist[i] = ((recup->ray.posx - recup->sxy[i].x) *
-				(recup->ray.posx - recup->sxy[i].x) + (recup->ray.posy -
-					recup->sxy[i].y) * (recup->ray.posy - recup->sxy[i].y));
+		recup->s.dist[i] = ((recup->ray.posx - recup->sxy[i].x)
+				* (recup->ray.posx - recup->sxy[i].x) + (recup->ray.posy
+					- recup->sxy[i].y) * (recup->ray.posy - recup->sxy[i].y));
 	}
 }
 
@@ -56,12 +56,12 @@ void	ft_calculs(t_recup *recup, int i)
 {
 	recup->s.spritex = recup->sxy[recup->s.order[i]].x - recup->ray.posx;
 	recup->s.spritey = recup->sxy[recup->s.order[i]].y - recup->ray.posy;
-	recup->s.invdet = 1.0 / (recup->ray.planx * recup->ray.diry -
-			recup->ray.dirx * recup->ray.plany);
-	recup->s.transformx = recup->s.invdet * (recup->ray.diry *
-			recup->s.spritex - recup->ray.dirx * recup->s.spritey);
-	recup->s.transformy = recup->s.invdet * (-recup->ray.plany *
-			recup->s.spritex + recup->ray.planx * recup->s.spritey);
+	recup->s.invdet = 1.0 / (recup->ray.planx * recup->ray.diry
+			- recup->ray.dirx * recup->ray.plany);
+	recup->s.transformx = recup->s.invdet * (recup->ray.diry
+			* recup->s.spritex - recup->ray.dirx * recup->s.spritey);
+	recup->s.transformy = recup->s.invdet * (-recup->ray.plany
+			* recup->s.spritex + recup->ray.planx * recup->s.spritey);
 	recup->s.spritescreenx = (int)((recup->rx / 2) * (1 + recup->s.transformx
 				/ recup->s.transformy));
 	recup->s.spriteheight = abs((int)(recup->ry / (recup->s.transformy)));
@@ -89,12 +89,12 @@ void	ft_draw_spr(t_recup *recup, int y, int texx, int stripe)
 	{
 		d = (y) * 256 - recup->ry * 128 + recup->s.spriteheight * 128;
 		texy = ((d * recup->texture[4].height) / recup->s.spriteheight) / 256;
-		if (recup->texture[4].addr[texy * recup->texture[4].line_length / 4 +
-				texx] != -16777216)
+		if (recup->texture[4].addr[texy * recup->texture[4].line_length / 4
+				+ texx] != -16777216)
 		{
-			recup->data.addr[y * recup->data.line_length / 4 + stripe] =
-				recup->texture[4].addr[texy * recup->texture[4].line_length /
-				4 + texx];
+			recup->data.addr[y * recup->data.line_length / 4 + stripe]
+				= recup->texture[4].addr[texy * recup->texture[4].line_length
+				/ 4 + texx];
 		}
 		y++;
 	}
@@ -102,10 +102,10 @@ void	ft_draw_spr(t_recup *recup, int y, int texx, int stripe)
 
 void	ft_sprite(t_recup *recup)
 {
-	int i;
-	int y;
-	int stripe;
-	int texx;
+	int	i;
+	int	y;
+	int	stripe;
+	int	texx;
 
 	i = -1;
 	ft_dist_order(recup);
@@ -115,11 +115,11 @@ void	ft_sprite(t_recup *recup)
 		stripe = recup->s.drawstartx;
 		while (stripe < recup->s.drawendx)
 		{
-			texx = (int)(256 * (stripe - (-recup->s.spritewidth / 2 +
-							recup->s.spritescreenx)) * recup->texture[4].width
+			texx = (int)(256 * (stripe - (-recup->s.spritewidth / 2
+							+ recup->s.spritescreenx)) * recup->texture[4].width
 					/ recup->s.spritewidth) / 256;
 			if (recup->s.transformy > 0 && stripe >= 0 && stripe < recup->rx
-					&& recup->s.transformy < recup->s.zbuffer[stripe])
+				&& recup->s.transformy < recup->s.zbuffer[stripe])
 			{
 				y = recup->s.drawstarty;
 				ft_draw_spr(recup, y, texx, stripe);
