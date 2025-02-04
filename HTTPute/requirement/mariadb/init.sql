12fef42421bae88d625dafe846b870a5e329271f
Axel lanty <alanty@localhost.localdomain>
2025-02-04 19:03:35 +0100

    hummmmmm jesus ?

--- HTTPute/requirement/mariadb/init.sql
+++ HTTPute/requirement/mariadb/init.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE WordFumer;
-CREATE USER 'wpuser'@'%' IDENTIFIED BY 'mot_de_passe';
+CREATE DATABASE IF NOT EXISTS WordFumer;
+CREATE USER IF NOT EXISTS 'wpuser'@'%' IDENTIFIED BY 'mot_de_passe';
 GRANT ALL PRIVILEGES ON WordFumer.* TO 'wpuser'@'%';
 FLUSH PRIVILEGES;
